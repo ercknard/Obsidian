9e668d122fa5269a00dde315d031d9ff30399cd7
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-03-19 16:47:36 +0800

    12

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,7 +1,29 @@
-
-function fadeOut() {
+const fadeOuts = () => {
     const loaderWrapper = document.querySelector('.wrapper');
     loaderWrapper.classList.add('fades');
   }
   
-  window.addEventListener('load',fadeOut);
+  window.addEventListener('load',fadeOuts);
+
+
+var loader = document.getElementById("loader");
+
+// Function to show the loader
+function showLoader() {
+  loader.style.display = "block";
+}
+
+// Function to hide the loader
+function hideLoader() {
+  loader.style.display = "none";
+}
+
+// Function to load some content
+function fadeOut() {
+  // Show the loader
+  showLoader();
+
+  // Simulate a delay to load some content
+    // Hide the loader
+    hideLoader();
+}
