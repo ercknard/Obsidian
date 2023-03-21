86863243ae405bd1cbc64bc5c80ff3e98b30a9d2
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-03-22 02:18:31 +0800

    12

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,4 +1,4 @@
 function setHalfVolume() {
     var myAudio = document.getElementById("audio1");  
-    myAudio.volume = 0.01; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
+    myAudio.volume = 0.01;
 }
--- scripts/tab.js
+++ scripts/tab.js
@@ -9,43 +9,43 @@ function openTab(tabName) {
   }
 
 
-// Get the container element
+
 var btnContainer = document.getElementById("home-active-btn");
 
-// Get all buttons with class="btn" inside the container
+
 var btns = btnContainer.getElementsByClassName("nav-buttons");
 
-// Loop through the buttons and add the active class to the current/clicked button
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
 
-    // If there's no active class
+
     if (current.length > 0) {
       current[0].className = current[0].className.replace(" active", "");
     }
 
-    // Add the active class to the current/clicked button
+
     this.className += " active";
   });
 }
 
 var btnContainercoin = document.getElementById("coin-active-btn");
 
-// Get all buttons with class="btn" inside the container
+
 var btnscoin = btnContainercoin.getElementsByClassName("coin-buttons");
 
-// Loop through the buttons and add the active class to the current/clicked button
+
 for (var i = 0; i < btnscoin.length; i++) {
   btnscoin[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active-coin");
 
-    // If there's no active class
+
     if (current.length > 0) {
       current[0].className = current[0].className.replace(" active-coin", "");
     }
 
-    // Add the active class to the current/clicked button
+
     this.className += " active-coin";
   });
 }
